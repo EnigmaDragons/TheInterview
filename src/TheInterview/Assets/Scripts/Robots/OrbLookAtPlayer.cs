463184bac605c00306b68b592f2d9491cb3bb769
SilasReinagel <silas.reinagel@gmail.com>
2020-05-09 14:48:01 -0700

    Camera Orbs always look at Player. Closes #77

---
+++ src/TheInterview/Assets/Scripts/Robots/OrbLookAtPlayer.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class OrbLookAtPlayer : MonoBehaviour
+{
+    private GameObject player;
+    
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.LookAt(player.transform);
+    }
+}
