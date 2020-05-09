095ac70ea5be76485f3bf4aa409191f2d9d1ac99
GraphicEdit <contact@graphicedit.com>
2020-05-09 16:54:40 +0100

    NeonSign with LightFlickerEffect

---
+++ src/TheInterview/Assets/Prefabs/room_APARTMENT/EmissiveOscillator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public sealed class EmissiveOscillator : MonoBehaviour 
+{
+   	
+     [SerializeField] public float frequency ;
+     [SerializeField] public float amplitude ;
+     [SerializeField] public Material material;
+     [SerializeField] public Color emissionColor;
+	 private float glow;
+ 
+
+     void Start () 
+     {
+		 emissionColor = material.GetColor("_EmissionColor");
+     }
+     
+
+     void Update () 
+     {
+         glow = (2 + Mathf.Cos(Time.time * frequency)) * amplitude;
+         material.SetColor("_EmissionColor", emissionColor * glow);
+     }
+}
