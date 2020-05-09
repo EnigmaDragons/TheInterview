b6fbdc77914f14406c1d955071d2952eed452a7d
SilasReinagel <silas.reinagel@gmail.com>
2020-05-08 21:57:46 -0700

    Added Stair Floor Numbers. Locked Doors except Floor 17. Closes #37. Closes #41

---
+++ src/TheInterview/Assets/Scripts/Stairs/FloorSizing.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public static class FloorSizing
+{
+    private static readonly int StartingFloorNumber = 3;
+    private static readonly int HeightPerFloor = 6;
+    
+    public static int FloorNumber(this Transform t) 
+        => Mathf.FloorToInt(t.position.y / HeightPerFloor + StartingFloorNumber);
+}
---
+++ src/TheInterview/Assets/Scripts/Stairs/SetDoorInteractable.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class SetDoorInteractable : MonoBehaviour
+{
+    [SerializeField] private int[] enabledFloors;
+    [SerializeField] private Door door;
+
+    private void Awake() 
+        => door.SetCanBeOpened(enabledFloors.Contains(door.transform.FloorNumber()));
+}
