a01e095b739067d9352d4fd27d12f334e38cfd13
SilasReinagel <silas.reinagel@gmail.com>
2020-05-09 00:26:45 -0700

    Added Delayed Game Reset. Closes #47

---
+++ src/TheInterview/Assets/Scripts/Endings/GameResetHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameResetHandler : OnMessage<ResetGameAfterDelay>
+{
+    [SerializeField] private FloatReference delay;
+    [SerializeField] private Navigator navigator;
+    
+    protected override void Execute(ResetGameAfterDelay msg) => StartCoroutine(Go());
+
+    private IEnumerator Go()
+    {
+        yield return new WaitForSeconds(delay);
+        navigator.GoToScene2();
+    }
+}
