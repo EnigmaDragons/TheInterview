5cb73bc8774e4eb3f9e804468e24c9db71f13773
noah reinagel <noah.r9@gmail.com>
2020-05-08 22:38:43 -0700

    halfway elevator interactable

---
+++ src/TheInterview/Assets/Scripts/Reactions/OnInteract.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class OnInteract : OnMessage<>
+{
+    [SerializeField] private Elevator elevator;
+
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+}
