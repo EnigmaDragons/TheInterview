a9f25c05f8826d6deb52f961ddc2c19a853cff98
SilasReinagel <silas.reinagel@gmail.com>
2020-05-10 14:45:57 -0700

    Setup initial Elevator Ride. Closes #109. Closes #61

--- src/TheInterview/Assets/Scripts/Elevator/InitElevatorState.cs
+++ src/TheInterview/Assets/Scripts/Elevator/InitElevatorState.cs
@@ -6,10 +6,13 @@ public class InitElevatorState : MonoBehaviour
     [SerializeField] private Elevator elevator;
     [SerializeField] private int startingFloor;
     [SerializeField] private int destinationFloor;
+    [SerializeField] private bool autoStartElevator;
     
     private void Start()
     {
         elevator.SetCurrentFloor(startingFloor);
         elevator.SetDestination(destinationFloor);
+        if (autoStartElevator)
+            elevator.TravelTo(destinationFloor);
     }
 }
