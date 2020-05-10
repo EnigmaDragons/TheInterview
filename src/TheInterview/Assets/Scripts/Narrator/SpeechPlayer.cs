df1591f881b588ea1d451f185ec6090531c8e91a
SilasReinagel <silas.reinagel@gmail.com>
2020-05-10 11:01:29 -0700

    Tagged all Speech content. Added Speech filter. Closes #117. Closes #118

--- src/TheInterview/Assets/Scripts/Narrator/SpeechPlayer.cs
+++ src/TheInterview/Assets/Scripts/Narrator/SpeechPlayer.cs
@@ -1,9 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 public class SpeechPlayer : OnMessage<PlaySpeech>
 {
+    [SerializeField] private NarratedBy[] includedVoiceActors;
+    
     protected override void Execute(PlaySpeech msg)
     {
+        if (!includedVoiceActors.Contains(msg.Speech.NarratedBy))
+        {
+            Debug.LogWarning($"Not Playing Requested Speech {msg.Speech.name}, since it is narrated by {msg.Speech.NarratedBy}");
+            return;
+        }
+        
         Debug.Log($"Playing Speech {msg.Speech.name}");
         StartCoroutine(msg.Speech.AsyncPlay());
     }
