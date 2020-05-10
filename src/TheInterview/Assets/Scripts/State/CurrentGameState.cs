b0252e62de6e600d914ad8f7ac10027ea8f99ce3
noah reinagel <noah.r9@gmail.com>
2020-05-09 20:39:28 -0700

    added a bunch of choices in the weakness room

--- src/TheInterview/Assets/Scripts/State/CurrentGameState.cs
+++ src/TheInterview/Assets/Scripts/State/CurrentGameState.cs
@@ -6,15 +6,22 @@ public sealed class CurrentGameState : ScriptableObject
 {
     [SerializeField] private GameState gameState;
     [SerializeField] private IntVariable[] counters;
+    [SerializeField] private IntVariable[] softCounters;
 
     public void Init()
     {
+        SoftReset();
         for (var i = 0; i < counters.Length; i++)
             counters[i].Value = 0;
         gameState = new GameState();
     }
 
     public void Init(GameState initialState) => gameState = initialState;
+    public void SoftReset()
+    {
+        for (var i = 0; i < softCounters.Length; i++)
+            softCounters[i].Value = 0;
+    }
     public void Subscribe(Action<GameStateChanged> onChange, object owner) => Message.Subscribe(onChange, owner);
     public void Unsubscribe(object owner) => Message.Unsubscribe(owner);
     
