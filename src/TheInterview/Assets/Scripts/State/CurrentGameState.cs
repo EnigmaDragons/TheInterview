3c3687d7618af71333edd9392f2555403385f3eb
noah reinagel <noah.r9@gmail.com>
2020-05-09 21:22:21 -0700

    made back end scripts for weakness scene

--- src/TheInterview/Assets/Scripts/State/CurrentGameState.cs
+++ src/TheInterview/Assets/Scripts/State/CurrentGameState.cs
@@ -6,22 +6,21 @@ public sealed class CurrentGameState : ScriptableObject
 {
     [SerializeField] private GameState gameState;
     [SerializeField] private IntVariable[] counters;
-    [SerializeField] private IntVariable[] softCounters;
 
     public void Init()
     {
         SoftReset();
         for (var i = 0; i < counters.Length; i++)
             counters[i].Value = 0;
-        gameState = new GameState();
     }
 
     public void Init(GameState initialState) => gameState = initialState;
+
     public void SoftReset()
     {
-        for (var i = 0; i < softCounters.Length; i++)
-            softCounters[i].Value = 0;
+        gameState = new GameState();
     }
+
     public void Subscribe(Action<GameStateChanged> onChange, object owner) => Message.Subscribe(onChange, owner);
     public void Unsubscribe(object owner) => Message.Unsubscribe(owner);
     
