a7a6e8e88e2f82eb3785e96560ba03b935170442
SilasReinagel <silas.reinagel@gmail.com>
2020-05-10 00:51:16 -0700

    Setup more Screen Fades. Closes #50. Closes #114

--- src/TheInterview/Assets/Scripts/Endings/GameResetHandler.cs
+++ src/TheInterview/Assets/Scripts/Endings/GameResetHandler.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameResetHandler : OnMessage<ResetGameAfterDelay>
 {
     [SerializeField] private FloatReference delay;
     [SerializeField] private Navigator navigator;
+    [SerializeField] private UnityEvent initialAction;
     
     protected override void Execute(ResetGameAfterDelay msg) => StartCoroutine(Go());
 
     private IEnumerator Go()
     {
+        initialAction.Invoke();
         yield return new WaitForSeconds(delay);
         navigator.GoToScene2();
     }
