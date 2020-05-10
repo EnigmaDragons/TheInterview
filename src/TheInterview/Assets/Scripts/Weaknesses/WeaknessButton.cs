c9a9a27a0cf3d3181ff5b79f3b8a7611352e7325
noah reinagel <noah.r9@gmail.com>
2020-05-09 21:45:20 -0700

    wired up weaknesses

---
+++ src/TheInterview/Assets/Scripts/Weaknesses/WeaknessButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class WeaknessButton : OnMessage<WeaknessSelected, Finished<WeaknessSelected>>
+{
+    [SerializeField] private Speech speech;
+    [SerializeField] private CurrentGameState gameState;
+
+    private bool _isWeaknessPlaying;
+
+    public void Click()
+    {
+        if (_isWeaknessPlaying)
+            return;
+        Message.Publish(new WeaknessSelected());
+        speech.Play();
+    }
+
+    protected override void Execute(WeaknessSelected msg)
+    {
+        _isWeaknessPlaying = true;
+    }
+
+    protected override void Execute(Finished<WeaknessSelected> msg)
+    {
+        _isWeaknessPlaying = false;
+    }
+}
---
+++ src/TheInterview/Assets/Scripts/Weaknesses/WeaknessRoom.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class WeaknessRoom : OnMessage<Finished<WeaknessSelected>>
+{
+    [SerializeField] private GameObject[] weaknessButtons;
+    [SerializeField] private int targetNumber = 3;
+    [SerializeField] private Door door;
+
+    private int _count;
+
+    protected override void Execute(Finished<WeaknessSelected> msg)
+    {
+        _count++;
+        if (_count == targetNumber)
+        {
+            for (var i = 0; i < weaknessButtons.Length; i++)
+                weaknessButtons[i].SetActive(false);
+            door.SetCanBeOpened(true);
+            door.OpenDoor();
+            door.SetCanBeOpened(false);
+        }
+    }
+}
