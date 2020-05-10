9c01b161a976cdfe0d36d583436ace0bfa642fa3
noah reinagel <noah.r9@gmail.com>
2020-05-10 00:52:56 -0700

    added lots of weakness stuff to the weakness room

---
+++ src/TheInterview/Assets/Scripts/Robots/OnHideOrbs.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class OnHideOrbs : OnMessage<HideOrbs>
+{
+    [SerializeField] private Vector3 destination;
+    [SerializeField] private float speed;
+
+    private bool _isHiding;
+
+    protected override void Execute(HideOrbs msg)
+    {
+        _isHiding = true;
+    }
+
+    private void Update()
+    {
+        if (!_isHiding)
+            return;
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, destination, speed * Time.deltaTime);
+        if (transform.localPosition == destination)
+            _isHiding = false;
+    }
+}
