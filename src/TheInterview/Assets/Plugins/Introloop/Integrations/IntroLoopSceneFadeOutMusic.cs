d7fe78eae1dea92fe9db3c574caf1e1fd443e1ee
SilasReinagel <silas.reinagel@gmail.com>
2020-05-09 20:22:55 -0700

    Created new Short Game scene. Closes #103. Closes #101. Closes #63

---
+++ src/TheInterview/Assets/Plugins/Introloop/Integrations/IntroLoopSceneFadeOutMusic.cs
@@ -0,0 +1,13 @@
+﻿using E7.Introloop;
+using UnityEngine;
+
+public class IntroLoopSceneFadeOutMusic : MonoBehaviour
+{
+    [SerializeField] private float fadeoutDuration = 3f;
+
+    void OnEnable()
+    {
+        Debug.Log("IntroLoop fadeout");
+        IntroloopPlayer.Instance.Stop(fadeoutDuration);
+    }
+}
