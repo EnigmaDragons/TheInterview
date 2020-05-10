377c5cc81ef71cb28ca2480002835e54bb5cc63f
SilasReinagel <silas.reinagel@gmail.com>
2020-05-10 16:58:34 -0700

    Created Basic Intro Cutscene First Draft. Closes #135

--- src/TheInterview/Assets/Scripts/Navigator.cs
+++ src/TheInterview/Assets/Scripts/Navigator.cs
@@ -7,6 +7,7 @@ public class Navigator : ScriptableObject
 {
     public void GoToShortGame() => LoadScene("ShortGame-FullScene");
     public void GoToMainMenu() => LoadScene("MainMenu");
+    public void GoToIntroCutscene() => LoadScene("SimpleIntroCutscene");
     public void GoToCredits() => LoadScene("SimpleCreditsScene");
     public void GoToScene2() => LoadScene("Scene2-UrbanamicsBuilding");
     public void GoToScene3() => LoadScene("Scene3-TheInterview");
