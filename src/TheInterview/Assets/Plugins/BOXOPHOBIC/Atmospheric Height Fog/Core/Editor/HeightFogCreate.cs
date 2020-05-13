a26d0576b689edd6b44b87ae38d07139006b6c80
SilasReinagel <silas.reinagel@gmail.com>
2020-05-12 19:25:29 -0700

    Added Skyline outside office building. Closes 151

---
+++ src/TheInterview/Assets/Plugins/BOXOPHOBIC/Atmospheric Height Fog/Core/Editor/HeightFogCreate.cs
@@ -0,0 +1,37 @@
+﻿// Cristian Pop - https://boxophobic.com/
+
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class HeightFogCreate
+{
+    [MenuItem("GameObject/BOXOPHOBIC/Atmospheric Height Fog/Global Volume", false, 9)]
+    static void CreateGlobalVolume()
+    {
+        if (GameObject.Find("Height Fog Global") != null)
+        {
+            Debug.Log("[Warning][Atmospheric Height Fog] " + "Height Fog Global is already added to your scene!");
+            return;
+        }
+
+        GameObject go = new GameObject();
+        go.AddComponent<HeightFogGlobal>();
+
+        Selection.activeGameObject = go;
+
+        EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+    }
+
+    [MenuItem("GameObject/BOXOPHOBIC/Atmospheric Height Fog/Override Volume", false, 9)]
+    static void CreateOverrideVolume()
+    {
+        GameObject go = new GameObject();
+        go.AddComponent<HeightFogOverride>();
+
+        Selection.activeGameObject = go;
+
+        EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+    }
+}
+
