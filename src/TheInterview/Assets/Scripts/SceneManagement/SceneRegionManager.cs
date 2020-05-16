9348de4d8ff3c15434faa210526d68dfef33235b
Silas Reinagel <silas.reinagel@carvana.com>
2020-05-16 10:23:05 -0700

    Tuned Scene Area manager to be more fiddle-resistant.

--- src/TheInterview/Assets/Scripts/SceneManagement/SceneRegionManager.cs
+++ src/TheInterview/Assets/Scripts/SceneManagement/SceneRegionManager.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class SceneRegionManager : OnMessage<DisableRegion, EnableRegion>
 {
     [SerializeField] private SceneArea[] areas;
+    [SerializeField] private SceneArea[] startingAreasEnabled;
 
     private Dictionary<string, GameObject> _regions;
 
-    private void Awake() => _regions = areas.ToDictionary(a => a.RegionName, a => a.Object);
-    
+    private void Awake()
+    {
+        _regions = areas.ToDictionary(a => a.RegionName, a => a.Object);
+        areas.ForEach(a => a.Object.SetActive(false));
+        startingAreasEnabled.ForEach(a => a.Object.SetActive(true));
+    }
+
     protected override void Execute(DisableRegion msg)
     {
         if (_regions.TryGetValue(msg.RegionName, out var area))
