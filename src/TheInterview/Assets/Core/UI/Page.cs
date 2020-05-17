04994b979d96713a6f121aa589cd5fd87f99d60f
Silas Reinagel <silas.reinagel@carvana.com>
2020-05-17 02:27:06 -0700

    Added More Genders and Degrees. Closes #238

--- src/TheInterview/Assets/Core/UI/Page.cs
+++ src/TheInterview/Assets/Core/UI/Page.cs
@@ -6,12 +6,15 @@ public sealed class Page : MonoBehaviour
 {
     [SerializeField] private Button nextButton;
     [SerializeField] private Button previousButton;
-
-    public void Init(Action prev, Action next)
+    [SerializeField] private Button actionButton;
+    
+    public void Init(Action prev, Action next, Action action)
     {
         if (previousButton != null)
             previousButton.onClick.AddListener(() => prev());
         if (nextButton != null)
             nextButton.onClick.AddListener(() => next());
+        if (actionButton != null)
+            actionButton.onClick.AddListener(() => action());
     }
 }
