8ab9879257140ca3a538c6813fafcac6cd58a60c
SilasReinagel <silas.reinagel@gmail.com>
2020-06-06 01:05:37 -0700

    Receive AR Notification with Acceptance message. Closes #303

---
+++ src/TheInterview/Assets/Scripts/Hud/ComplexProgerssBar.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Knife.UI
+{
+    [ExecuteAlways]
+    public class ComplexProgerssBar : MonoBehaviour
+    {
+        [SerializeField] private Image[] parts;
+        [SerializeField] [Range(0, 1)] private float fraction;
+        [SerializeField] private bool whole;
+
+        public float Fraction
+        {
+            get => fraction;
+            set
+            {
+                fraction = value;
+                UpdateParts();
+            }
+        }
+
+        public Image[] Parts { get => parts; set => parts = value; }
+
+        private void OnValidate()
+        {
+            UpdateParts();
+        }
+
+        private void UpdateParts()
+        {
+            if(Parts != null)
+            {
+                float indexDeltaFraction = 1f / Parts.Length;
+
+                for(int i = 0; i < Parts.Length; i++)
+                {
+                    float indexFraction = indexDeltaFraction * i;
+                    float nextIndexFraction = indexFraction + indexDeltaFraction;
+
+                    float localFraction = Mathf.InverseLerp(indexFraction, nextIndexFraction, fraction);
+
+                    if (whole)
+                    {
+                        Parts[i].enabled = localFraction >= 0.5f;
+                    }
+                    else
+                    {
+                        Parts[i].fillAmount = localFraction;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/TheInterview/Assets/Scripts/Hud/OnHudFocused.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class OnHudFocused : OnMessage<GameStateChanged>
+{
+    [SerializeField] private UnityEvent action;
+    
+    protected override void Execute(GameStateChanged msg)
+    {
+        if (!msg.State.HudIsFocused)
+            return;
+        
+        action.Invoke();
+        enabled = false;
+    }
+}
