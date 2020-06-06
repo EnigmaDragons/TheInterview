3aafc13d8432269cb654b8f9a5be4ce1b71d800d
SilasReinagel <silas.reinagel@gmail.com>
2020-06-06 13:56:26 -0700

    Setup UI Widget Opacity. Closes #320

---
+++ src/TheInterview/Assets/Scripts/Hud/HudCanvasGroupOpacityFactor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class HudCanvasGroupOpacityFactor : OnMessage<GameStateChanged>
+{
+    [SerializeField] private float focusedOpacity = 1f;
+    [SerializeField] private float unfocusedOpacity = 0f;
+
+    private CanvasGroup _group;
+    
+    private void Awake()
+    {
+        _group = GetComponent<CanvasGroup>();
+        UpdateOpacity(false);
+    }
+
+    private void UpdateOpacity(bool isFocused) 
+        => _group.alpha = isFocused ? focusedOpacity : unfocusedOpacity;
+
+    protected override void Execute(GameStateChanged msg) 
+        => UpdateOpacity(msg.State.HudIsFocused);
+}
