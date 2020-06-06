6226182f754b3bb428cdbc5caba9274ed43f31f6
SilasReinagel <silas.reinagel@gmail.com>
2020-06-06 11:40:16 -0700

    App UI activate after install. Closes #318

---
+++ src/TheInterview/Assets/Scripts/App/OnAppInstalled.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class OnAppInstalled : OnMessage<AppStateChanged>
+{
+    [SerializeField] private UnityEvent action;
+
+    private bool _finished;
+    
+    protected override void Execute(AppStateChanged msg)
+    {
+        if (_finished || !msg.AppState.AppInstalled)
+            return;
+
+        _finished = true;
+        action.Invoke();
+    }
+}
