de78b7505cb6b30023dff9469082ae32742d5b85
noah reinagel <noah.r9@gmail.com>
2020-06-07 16:41:18 -0700

    finished the refuse ending

---
+++ src/TheInterview/Assets/Scripts/Buttons/Button1.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Button1 : MonoBehaviour
+{
+    [SerializeField] private Sprite blueBtn;
+    [SerializeField] private Sprite redBtn;
+    [SerializeField] private Sprite blueLight;
+    [SerializeField] private Sprite redLight;
+    [SerializeField] private Image btn;
+    [SerializeField] private Image light;
+
+    public Button Button;
+
+    public void SetToBlue()
+    {
+        btn.sprite = blueBtn;
+        light.sprite = blueLight;
+    }
+
+    public void SetToRed()
+    {
+        btn.sprite = redBtn;
+        light.sprite = redLight;
+    }
+}
