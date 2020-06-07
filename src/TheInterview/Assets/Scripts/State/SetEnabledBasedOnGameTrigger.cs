ae2cbc0414543b614e200654c94e82c801e4ae43
SilasReinagel <silas.reinagel@gmail.com>
2020-06-07 14:24:50 -0700

    Allow Object management by Game Trigger. Closes #372. Closes #366

---
+++ src/TheInterview/Assets/Scripts/State/SetEnabledBasedOnGameTrigger.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class SetEnabledBasedOnGameTrigger : OnMessage<GameStateChanged>
+{
+    [SerializeField] private GameObject target;
+    [SerializeField] private StringReference trigger;
+    [SerializeField] private bool shouldBeEnabledIfTriggered = true;
+    
+    protected override void Execute(GameStateChanged msg)
+    {
+        var isTriggered = msg.State.IsTriggered(trigger.Value);
+        target.SetActive(shouldBeEnabledIfTriggered ? isTriggered : !isTriggered);
+    }
+}
