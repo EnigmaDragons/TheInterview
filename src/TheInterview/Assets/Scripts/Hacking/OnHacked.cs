6927b011209b3cf15471566c7d4fe02c83db6400
noah reinagel <noah.r9@gmail.com>
2020-06-07 17:55:43 -0700

    wired up new objectives and hacking B3

---
+++ src/TheInterview/Assets/Scripts/Hacking/OnHacked.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class OnHacked : OnMessage<GameStateChanged>
+{
+    [SerializeField] private CurrentGameState currentGameState;
+    [SerializeField] private StringReference hackedKey;
+    [SerializeField] private Trigger trigger;
+
+    private bool _hasBeenHacked;
+
+    private void Awake() => _hasBeenHacked = currentGameState.HasTriggeredThisRun(hackedKey.Value);
+
+    protected override void Execute(GameStateChanged msg)
+    {
+        if (!_hasBeenHacked && currentGameState.HasTriggeredThisRun(hackedKey.Value))
+        {
+            _hasBeenHacked = true;
+            trigger.Execute();
+        }
+    }
+}
