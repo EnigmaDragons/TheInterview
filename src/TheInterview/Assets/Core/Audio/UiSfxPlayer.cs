83bad8b2f5e493a64825390b27be39ca28b9dd28
SilasReinagel <silas.reinagel@gmail.com>
2020-06-07 17:19:31 -0700

    Plays Money Gained Sound. Closes #347

--- src/TheInterview/Assets/Core/Audio/UiSfxPlayer.cs
+++ src/TheInterview/Assets/Core/Audio/UiSfxPlayer.cs
@@ -7,6 +7,7 @@ public sealed class UiSfxPlayer : ScriptableObject
 
     public void Init(AudioSource src) => source = src;
     public void InitIfNeeded(AudioSource src) => source.IfNull(() => Init(src));
+    public void Play(AudioClipVolume c) => Play(c.clip, c.volume);
     public void Play(AudioClip c, float volume = 1f)
     {
         if (source != null)
--- src/TheInterview/Assets/Scripts/Hud/CurrencyDisplay.cs
+++ src/TheInterview/Assets/Scripts/Hud/CurrencyDisplay.cs
@@ -5,6 +5,8 @@ public class CurrencyDisplay : OnMessage<AppStateChanged>
 {
     [SerializeField] private CurrentAppState appState;
     [SerializeField] private TextMeshProUGUI display;
+    [SerializeField] private UiSfxPlayer sfx;
+    [SerializeField] private AudioClipVolume moneySound;
 
     private void Start() => UpdateDisplay(appState.State.Creds);
     protected override void Execute(AppStateChanged msg) => UpdateDisplay(msg.AppState.Creds);
@@ -12,5 +14,7 @@ public class CurrencyDisplay : OnMessage<AppStateChanged>
     private void UpdateDisplay(int amount)
     {
         display.text = amount.ToString();
+        if (amount > 0)
+            sfx.Play(moneySound);
     }
 }
