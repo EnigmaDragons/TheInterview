13ccd9e2be7c03f99c9d2ad7389ff81d22a46c7a
SilasReinagel <silas.reinagel@gmail.com>
2020-06-21 17:24:38 -0700

    Shows Ending Completion on Ending Screen. Closes #467

---
+++ src/TheInterview/Assets/Scripts/Endings/EndingProgressUI.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class EndingProgressUI : MonoBehaviour
+{
+    [SerializeField] private AllEndings endings;
+    [SerializeField] private CurrentGameState game;
+    [SerializeField] private TextMeshProUGUI display;
+
+    private void Awake() 
+        => display.text = $"Endings Achieved:\n{game.ReadOnly.PermanentCounters["EndingsCompleted"]} / {endings.Count}";
+}
