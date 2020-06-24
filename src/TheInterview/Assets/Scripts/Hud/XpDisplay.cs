7a4e36eac5ac22c1884801d13e3a6c53fba53bb9
SilasReinagel <silas.reinagel@gmail.com>
2020-06-23 21:49:31 -0700

    XP UI now updates. Closes #482

---
+++ src/TheInterview/Assets/Scripts/Hud/XpDisplay.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class XpDisplay : OnMessage<AppStateChanged>
+{
+    [SerializeField] private CurrentAppState appState;
+    [SerializeField] private TextMeshProUGUI display;
+
+    private void Start() => UpdateDisplay(appState.State);
+    protected override void Execute(AppStateChanged msg) => UpdateDisplay(msg.AppState);
+    
+    private void UpdateDisplay(AppState a)
+    {
+        display.text = $"{a.CurrentLevelXp}/{a.NextLevelXp}";
+    }
+}
