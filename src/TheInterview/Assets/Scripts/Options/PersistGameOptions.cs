aca0cca2c6e5f7bd963e565f897a58bbe2f74da5
SilasReinagel <silas.reinagel@gmail.com>
2020-06-23 23:28:22 -0700

    Fixed weird Lighting Shadows. Closes #457

--- src/TheInterview/Assets/Scripts/Options/PersistGameOptions.cs
+++ src/TheInterview/Assets/Scripts/Options/PersistGameOptions.cs
@@ -4,11 +4,11 @@ public class PersistGameOptions : OnMessage<GameOptionsChanged>
 {
     [SerializeField] private CurrentGameOptions options;
 
-    private static readonly string HideSubtitlesKey = "HideSubtitles";
+    private static readonly string ShowSubtitlesKey = "HideSubtitles";
     
     public void Awake() 
-        => options.Init(new GameOptions { ShowSubtitles = PlayerPrefs.GetInt(HideSubtitlesKey) == 0});
+        => options.Init(new GameOptions { ShowSubtitles = PlayerPrefs.GetInt(ShowSubtitlesKey) == 1});
 
     protected override void Execute(GameOptionsChanged msg) 
-        => PlayerPrefs.SetInt(HideSubtitlesKey, msg.Options.ShowSubtitles ? 0 : 1);
+        => PlayerPrefs.SetInt(ShowSubtitlesKey, msg.Options.ShowSubtitles ? 1 : 0);
 }
